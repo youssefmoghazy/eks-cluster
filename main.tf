158af6cbd13df554238d39059490afe837f025a7
Youssef Moghazy <youssefmoghazy55@gmail.com>
2026-06-17 21:08:04 +0300

    pipeline is updated 2

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 
 module "network" {
   source               = "./modules/network"
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  availability_zones   = var.availability_zones 
+  private_subnet_cidrs = var.private_subnet_cidrs
 }
+
+
