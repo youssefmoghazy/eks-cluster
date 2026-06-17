58c8e15f2661d9494e2e1fbf034d9c30dfcfba35
Youssef Moghazy <youssefmoghazy55@gmail.com>
2026-06-17 21:09:22 +0300

    pipeline is updated 3

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "public_subnet_cidrs" {
   description = "Public subnet CIDRs"
   type        = list(string)
   default     = [
-    "10.0.1.0/24"
+    "10.0.1.0/24",
     "10.0.2.0/24"
   ]
 }
